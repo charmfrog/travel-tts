0074cf9ade3db2d4a78841735ca81015a7f1105b
charmfrog <damink6114@gmail.com>
2026-07-21 02:04:18 +0900

    Add files via upload

---
+++ travel-tts-sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'travel-tts-v2';
+const APP_FILES = [
+  './vllo-tts-converter.html',
+  './travel-tts.webmanifest',
+  './travel-tts-icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(
+    keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
